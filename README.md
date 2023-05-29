0ca1747feaab0a5934711dd35e79420195d830a0
YouSH42 <enddl402@gmail.com>
2023-05-29 17:05:11 +0900

    README chage

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # 컴공창 기말 프로젝트
 
 #### C언어를 이용한 파일처리 함수
- - 
+ - fopen
+ - fclose
+ - fprintf
+ - fscanf
+ - fwrite
+ - fread
+ - fseek
+
+## fopen
+ - 기본적인 파일을 열어주는 함수
+ 사용법
+ ```
+	FILE *fopen(const char *filename, const char *mode)
+ ```
+	 
 
--- binary/project.c
+++ binary/project.c
@@ -8,7 +8,7 @@
 #define MAX_HP 65536
 #define ALLIANCE 256
 
-int is_text_file(const char *filename) {
+int is_text_file(const char *filename) {		//txt파일인지 아닌지 확인하는 함수
 	const char *extension = strrchr(filename, '.');
 	if (extension != NULL && strcmp(extension, ".txt") == 0) {
 		return 1; // 텍스트 파일인 경우
@@ -16,7 +16,7 @@ int is_text_file(const char *filename) {
 	return 0; // 텍스트 파일이 아닌 경우
 }
 
-int show_select(){
+int show_select(){	//선택창 및 선택한 값 리턴 함수
 	int input;
 	printf("1. 신규 플레이어 정보 기록\n");
 	printf("2. 기록된 모든 플레이어 정보 확인\n");
@@ -28,18 +28,18 @@ int show_select(){
 	return input;
 }
 
-void player_info_record(){
-	int i, age, hp, all_cnt = 0;
-	double x, y;
+void player_info_record(){	//플레이어 정보 입력 받는 함수
+	int i, age, hp, all_cnt = 0;		//플레이어 정보 
+	double x, y;										//플레이어 정보
 	char ch = 'Y', pl_ID[MAX_SIZE], alliance[ALLIANCE][MAX_SIZE],\
-						fl_name[MAX_SIZE], tmp[MAX_SIZE];
+						fl_name[MAX_SIZE], tmp[MAX_SIZE];		//플레이어 정보
 	FILE *fp = NULL;
-
+	//플레이어 정보 입력 받는 코드
 	printf("ID 를 입력하세요:");
 	scanf("%s%*c", pl_ID);
-	strcpy(tmp, pl_ID);
-	strcat(tmp, ".txt");
-	if((fp = fopen(tmp, "ab")) == NULL){
+	strcpy(tmp, pl_ID);			//임시파일에 아이디를 입력 받음
+	strcat(tmp, ".txt");		//.txt를 붙여서 텍스트 파일 이름으로 변경
+	if((fp = fopen(tmp, "wb")) == NULL){	// 아이디로 파일 생성
 		fprintf(stderr, "원본 파일을 열 수 없습니다.\n");
 		exit(1);
 	}
@@ -51,7 +51,7 @@ void player_info_record(){
 	scanf("%lf", &x);
 	printf("Y 좌표를 입력하세요:");
 	scanf("%lf", &y);
-	while(ch == 'Y'){
+	while(ch == 'Y'){		// Y가 들어오면 계속 입력 받음
 		printf("동맹 ID 를 입력하세요:");
 		scanf("%s%*c", alliance[all_cnt]);
 		all_cnt++;
@@ -59,6 +59,7 @@ void player_info_record(){
 		ch = getchar();
 		scanf("%*c");		//\n의 값을 무시하는 함수
 	}
+	// 파일에 바이너리 형식으로 입력
 	fwrite(pl_ID, sizeof(char), MAX_SIZE, fp);
 	fwrite(&age, sizeof(int), 1, fp);
 	fwrite(&hp, sizeof(int), 1, fp);
@@ -71,11 +72,11 @@ void player_info_record(){
 	fclose(fp);
 }
 
-int find_user_cnt(){
-	const char *folder_path = "./"; // 폴더 경로를 지정합니다
+int find_user_cnt(){		//플레이어가 몇명이 있는지 확인 후 그 숫자만큼 리턴
+	const char *folder_path = "./"; // 폴더 경로를 지정
 	DIR *dir;
 	struct dirent *entry;
-	int user_cnt = 0;
+	int user_cnt = 0;		//플레이어 수
 
 	dir = opendir(folder_path);
 	if (dir == NULL) {
@@ -84,40 +85,40 @@ int find_user_cnt(){
 	}
 
 	while ((entry = readdir(dir)) != NULL) {
-		if (entry->d_type == DT_REG) { // 일반 파일인 경우만 처리합니다
+		if (entry->d_type == DT_REG) { // 일반 파일인 경우만 처리
 			if (is_text_file(entry->d_name)) {
-				user_cnt++;
+				user_cnt++;	//txt 파일 일 경우 플레이어 파일로 간주 후 증가시킴
 			}
 		}
 	}
 	closedir(dir);
 	return user_cnt;
 }
-
+//플레이어 정보를 출력하는 함수
 void player_info_check(){
-	const char *folder_path = "./"; // 폴더 경로를 지정합니다
+	const char *folder_path = "./"; // 폴더 경로 지정
 	DIR *dir;
 	struct dirent *entry;
-	int age, hp, all_cnt, user_cnt, i, k = 1;
-	double x, y;
+	int age, hp, all_cnt, user_cnt, i, k = 1;		//플레이어 정보를 입력 받을 변수
+	double x, y;		//플레이어 정보를 입력 받을 변수
 	char pl_ID[MAX_SIZE], tmp[MAX_SIZE], alliance[ALLIANCE][MAX_SIZE];
+	//플레이어 정보를 입력 받을 변수
 	FILE *fp = NULL;
 
-	user_cnt = find_user_cnt();
+	user_cnt = find_user_cnt();	//플레이어 수를 확인
 	printf("총 %d명의 플레이어가 기록되어 있습니다.\n", user_cnt);
-
-	dir = opendir(folder_path);
+	dir = opendir(folder_path);		//디렉토리 오픈
 	if (dir == NULL) {
 		printf("폴더 열기 실패\n");
 		return ;
 	}
-	while ((entry = readdir(dir)) != NULL) {
+	while ((entry = readdir(dir)) != NULL) {	//모든 파일 검사
 		if (entry->d_type == DT_REG) { // 일반 파일인 경우만 처리합니다
 			if (is_text_file(entry->d_name)) {
 				if((fp = fopen(entry->d_name, "rb")) == NULL){
 					fprintf(stderr,"파일을 열 수 없습니다.\n");
 					exit(1);
-				}	
+				}		//바이너리 정보를 읽어와서 변수에 저장
 				fread(pl_ID, sizeof(char), MAX_SIZE, fp);
 				fread(&age, sizeof(int), 1, fp);
 				fread(&hp, sizeof(int), 1, fp);
@@ -140,16 +141,16 @@ void player_info_check(){
 	printf("완료되었습니다.\n");
 	closedir(dir);
 }
-
+// 플레이어 제거
 void player_delete(){
 	char name[MAX_SIZE], tmp[MAX_SIZE];
 
-	printf("삭제할 ID를 입력하세요: ");
+	printf("삭제할 ID를 입력하세요: ");	//삭제할 id 입력 받음
 	scanf("%s%*c", name);
-	strcpy(tmp, name);
-	strcat(tmp,".txt");
-	const char *folder_path = "./"; // 폴더 경로를 지정합니다
-	const char *search_name = tmp; // 검색할 파일 이름을 지정합니다
+	strcpy(tmp, name);		//임시 파일에 id 복사
+	strcat(tmp,".txt");	//임시 파일에 txt를 붙여서 검사할 수 있게 만듬
+	const char *folder_path = "./"; // 폴더 경로 지정
+	const char *search_name = tmp; // 검색할 파일 이름 지정
 	DIR *dir;
 	struct dirent *entry;
 
@@ -158,21 +159,21 @@ void player_delete(){
 		printf("폴더 열기 실패\n");
 		return;
 	}
-	while ((entry = readdir(dir)) != NULL) {
+	while ((entry = readdir(dir)) != NULL) {	//파일 전부 다 검사
 		if (entry->d_type == DT_REG) { // 일반 파일인 경우만 검색합니다
-			if (strcmp(entry->d_name, search_name) == 0) {
+			if (strcmp(entry->d_name, search_name) == 0) {	//해당하는 파일 검사
 				printf("%s 삭제완료.\n", name);
-				remove(search_name);
+				remove(search_name);	// 해당하는 파일 삭제
 				break;
 			}
 		}
 	}
 	closedir(dir);
-	if (entry == NULL) {
+	if (entry == NULL) {		//만약 삭제한 파일이 없다면
 		printf("해당 데이터는 존재하지 않습니다.\n");
 	}
 }
-
+// 사용자의 설정 명령을 받아서 해당하는 함수로 보내주는 함수
 void start_program(int input){
 	while(1){
 		switch(input){
